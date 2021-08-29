e3ce3625b6d8a59cbfbe9cf8f6823a09fe8b9346
Paris Nakita Kejser <contact@parisnakitakejser.com>
2021-08-29 18:09:32 +0200

    update stock for hook request

--- WMS/Api/StockChanged.php
+++ WMS/Api/StockChanged.php
@@ -2,15 +2,47 @@
 
 namespace SmartPack\WMS\Api;
 
+use SmartPack\Framework\Product;
+
 class StockChanged
 {
+    function __construct(
+        \Magento\Framework\Webapi\Rest\Request $request
+    ) {
+        $this->request = $request;
+    }
+
     /**
      * @inheritdoc
      */
     function productStockChanged()
     {
-        return [
-            'message' => 'hook ready to new code!'
-        ];
+        $body = json_decode($this->request->getContent());
+
+        $products = new Product();
+
+        $changed_data = [];
+        foreach ($body as $key => $val) {
+            $changed_data[] = [
+                'sku' => $val->sku,
+                'stock' => $val->totalCombined
+            ];
+
+            $products->updateEntity('catalog_product_entity', [
+                "wms_state" => 'pending',
+            ], ['sku = ?' => $val->sku]);
+
+            $objectManager = \Magento\Framework\App\ObjectManager::getInstance();
+            $stockRegistry = $objectManager->get('\Magento\CatalogInventory\Api\StockRegistryInterface');
+
+            $qty = $val->totalCombined;
+
+            $stockItem = $stockRegistry->getStockItemBySku($val->sku);
+            $stockItem->setQty($qty);
+            $stockItem->setIsInStock((bool)$qty); // this line
+            $stockRegistry->updateStockItemBySku($val->sku, $stockItem);
+        }
+
+        return $changed_data;
     }
 }
